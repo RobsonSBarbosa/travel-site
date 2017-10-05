8f132189608d7e73a8123bd29cd77ed420d29623
Robson <robsonstc@hotmail.com>
2017-10-04 23:53:20 -0300

    Completed svg png support for older browsers

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,1 +1,3 @@
+import "../../temp/scripts/modernizr";
 import 'lazysizes';
+import 'picturefill'; //browser compability, older browsers load srcset and picture attributes (no other programming necessary)
